cf80b2cf8535a4b1cc25d0bd08f6a03f2d513d1f
Mykolaj <32041427+MykolajKrusser@users.noreply.github.com>
2018-04-27 19:00:15 +0200

    deploy on  my own VPS server

--- server.js
+++ server.js
@@ -1,6 +1,18 @@
-const express = require('express')
-const app = express()
-
-app.use(express.static('public'))
-
-app.listen(process.env.PORT || 8080, () => console.log('All is OK'))
+/**
+ * Created by Mykolaj Krusser on 27/03/2018.
+ */
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const PORT = 3000;
+const api = require('./routes/api');
+const app = express();
+app.use(cors());
+app.use(bodyParser.json());
+app.use('/api', api);
+app.get('/', function(req, res){
+    res.send('Hello from server!!');
+});
+app.listen(PORT, function(){
+    console.log('Server running on localserever:' + PORT);
+});
